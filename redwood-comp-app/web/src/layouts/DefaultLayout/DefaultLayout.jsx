98067ee7a52d4c6c5ead59fc36348b3c01444a53
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-08-25 15:16:17 -0500

    Added admin role, and mvp tailwind to DefaultLayout

--- redwood-comp-app/web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ redwood-comp-app/web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,5 +1,4 @@
 import { Link, routes } from '@redwoodjs/router'
-
 import { useAuth } from 'src/auth'
 
 const DefaultLayout = ({ children }) => {
@@ -7,37 +6,41 @@ const DefaultLayout = ({ children }) => {
 
   return (
     <>
-      <header>
-        <div className="flex-between">
-          <h1>
-            <Link to={routes.home()}>Redwood</Link>
+      <header className="bg-blue-600 p-4 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">
+            <Link to={routes.home()} className="hover:underline">Redwood</Link>
           </h1>
-          {isAuthenticated ? (
-            <div>
-              <span>Logged in as {currentUser.email}</span>{' '}
-              <button type="button" onClick={logOut}>
+          {isAuthenticated && currentUser ? (
+            <div className="flex items-center">
+              <span className="mr-4">Logged in as {currentUser.email}</span>
+              <button 
+                type="button" 
+                onClick={logOut} 
+                className="px-4 py-2 bg-red-500 hover:bg-red-700 rounded transition-colors duration-300"
+              >
                 Logout
               </button>
             </div>
           ) : (
-            <Link to={routes.login()}>Login</Link>
+            <Link to={routes.login()} className="hover:underline">Login</Link>
           )}
         </div>
-        <nav>
-          <ul>
+        <nav className="mt-2">
+          <ul className="container mx-auto flex space-x-4">
             <li>
-              <Link to={routes.home()}>Home</Link>
+              <Link to={routes.home()} className="hover:underline">Home</Link>
             </li>
             <li>
-              <Link to={routes.about()}>About</Link>
+              <Link to={routes.about()} className="hover:underline">About</Link>
             </li>
             <li>
-              <Link to={routes.contact()}>Contact</Link>
+              <Link to={routes.contact()} className="hover:underline">Contact</Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="container mx-auto mt-6 p-4">{children}</main>
     </>
   )
 }
