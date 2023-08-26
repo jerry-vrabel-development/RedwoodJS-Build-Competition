5cfe676be749b41e1864afb3f924fae93b6546c2
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-08-26 17:00:38 -0500

    updated nav for auth routes

--- redwood-comp-app/web/src/components/Nav.jsx
+++ redwood-comp-app/web/src/components/Nav.jsx
@@ -3,35 +3,40 @@ import { useAuth } from 'src/auth'
 
 const Nav = () => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
+  const isAdmin = isAuthenticated && currentUser && currentUser.roles && currentUser.roles.includes("admin");
+
 
   return (
     <header className="bg-blue-600 p-4 text-white">
-    <div className="container mx-auto flex justify-between items-center">
-      <div className="flex items-center space-x-4">
-        <h1 className="text-xl font-bold">
-          <Link to={routes.home()} className="hover:underline">Redwood</Link>
-        </h1>
-        <Link to={routes.home()} className="hover:underline">Home</Link>
-        <Link to={routes.about()} className="hover:underline">About</Link>
-        <Link to={routes.contact()} className="hover:underline">Contact</Link>
-      </div>
-
-      {isAuthenticated && currentUser ? (
+      <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
-          <span>Logged in as {currentUser.email}</span>
-          <button 
-            type="button" 
-            onClick={logOut} 
-            className="px-4 py-2 bg-red-500 hover:bg-red-700 rounded transition-colors duration-300"
-          >
-            Logout
-          </button>
+          <h1 className="text-xl font-bold">
+            <Link to={routes.home()} className="hover:underline">Redwood</Link>
+          </h1>
+          <Link to={routes.home()} className="hover:underline">Home</Link>
+
+          {isAdmin && <Link to={routes.admin()} className="hover:underline">Admin</Link>}
+
+          <Link to={routes.about()} className="hover:underline">About</Link>
+          <Link to={routes.contact()} className="hover:underline">Contact</Link>
         </div>
-      ) : (
-        <Link to={routes.login()} className="hover:underline">Login</Link>
-      )}
-    </div>
-  </header>
+
+        {isAuthenticated && currentUser ? (
+          <div className="flex items-center space-x-4">
+            <span>Logged in as {currentUser.roles}</span>
+            <button
+              type="button"
+              onClick={logOut}
+              className="px-4 py-2 bg-red-500 hover:bg-red-700 rounded transition-colors duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link to={routes.login()} className="hover:underline">Login</Link>
+        )}
+      </div>
+    </header>
   )
 }
 
