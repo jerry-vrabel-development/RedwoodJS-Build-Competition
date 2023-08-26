03c7d2232f5f98be8834029defb2973f00b70935
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-08-26 15:29:55 -0500

    Added adminPage component and abstracted Nav from header component

---
+++ redwood-comp-app/web/src/components/Nav.jsx
@@ -0,0 +1,38 @@
+import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from 'src/auth'
+
+const Nav = () => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
+  return (
+    <header className="bg-blue-600 p-4 text-white">
+    <div className="container mx-auto flex justify-between items-center">
+      <div className="flex items-center space-x-4">
+        <h1 className="text-xl font-bold">
+          <Link to={routes.home()} className="hover:underline">Redwood</Link>
+        </h1>
+        <Link to={routes.home()} className="hover:underline">Home</Link>
+        <Link to={routes.about()} className="hover:underline">About</Link>
+        <Link to={routes.contact()} className="hover:underline">Contact</Link>
+      </div>
+
+      {isAuthenticated && currentUser ? (
+        <div className="flex items-center space-x-4">
+          <span>Logged in as {currentUser.email}</span>
+          <button 
+            type="button" 
+            onClick={logOut} 
+            className="px-4 py-2 bg-red-500 hover:bg-red-700 rounded transition-colors duration-300"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <Link to={routes.login()} className="hover:underline">Login</Link>
+      )}
+    </div>
+  </header>
+  )
+}
+
+export default Nav
