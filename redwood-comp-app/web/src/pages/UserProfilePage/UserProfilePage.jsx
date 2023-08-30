2dc5d5cf75563d3529a81999b76fa3e880cd1f97
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-08-29 19:20:16 -0500

    scaffold user and add routes

---
+++ redwood-comp-app/web/src/pages/UserProfilePage/UserProfilePage.jsx
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const UserProfilePage = () => {
+  return (
+    <>
+      <MetaTags title="User Profile" description="User Profile page" />
+      User Profile Page
+    </>
+  )
+}
+
+export default UserProfilePage
