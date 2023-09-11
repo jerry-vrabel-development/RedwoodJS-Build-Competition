8858982fa5b075a016060d953201173f4afacaa0
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-09-10 23:21:46 -0500

    added Footer component to defaultLayout.jsx

---
+++ redwood-comp-app/web/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-800 text-white p-4 mt-4 text-center">
+      © 2023 Your Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
