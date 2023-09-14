887b50db5b864b4d42c6ee89ca2b6b873d64a379
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-09-13 22:33:53 -0500

    refractored

--- redwood-comp-app/web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ redwood-comp-app/web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -7,19 +7,22 @@ const DefaultLayout = ({ children }) => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
 
   return (
-  <>
-    <header className="pl-6 bg-blue-600 text-white">
-    <h1 className="flex items-center text-xl font-bold">
-            <Link to={routes.home()} className="flex items-center text-blue-100 transition-colors duration-300 hover:text-orange-600">
-              Redwood
-            </Link>
-          </h1>
-      <Nav />
-    </header>
-    <main className="container mx-auto mt-6 p-4">{children}</main>
-    <Footer />
-  </>
+    <>
+      <header className="bg-blue-600 pl-6 text-white">
+        <h1 className="flex items-center text-xl font-bold">
+          <Link
+            to={routes.home()}
+            className="flex items-center text-blue-100 transition-colors duration-300 hover:text-orange-600"
+          >
+            Redwood
+          </Link>
+        </h1>
+        <Nav />
+      </header>
+      <main className="container mx-auto mt-6 p-4">{children}</main>
+      <Footer />
+    </>
   )
 }
 
-export default DefaultLayout;
+export default DefaultLayout
