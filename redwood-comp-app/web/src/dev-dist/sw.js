653c8db7db5f033c659de40d06d726bd8ec62423
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-09-15 22:02:13 -0500

    service worker

--- redwood-comp-app/web/src/dev-dist/sw.js
+++ redwood-comp-app/web/src/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.49dj4ahbjdg"
+    "revision": "0.pouvvka3q2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
